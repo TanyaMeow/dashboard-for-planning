3fb6506da9d546a1c8e3790a24fd04384768c9b0
thonyakina <thonyakina@zuzex.com>
2024-02-28 13:36:43 +0300

    Добавлены колонки доя таблицы

--- src/Components/UserTable.tsx
+++ src/Components/UserTable.tsx
@@ -1,9 +1,44 @@
 import {Table} from "antd";
 
+const columns = [
+    {
+        title: 'Name',
+        dataIndex: 'name',
+        key: 'name'
+    },
+    {
+        title: 'Surname',
+        dataIndex: 'surname',
+        key: 'surname'
+    },
+    {
+        title: 'Date of birth',
+        dataIndex: 'dateOfBirth',
+        key: 'dateOfBirth'
+    },
+    {
+        title: 'Email',
+        dataIndex: 'email',
+        key: 'email'
+    },
+    {
+        title: 'Phone',
+        dataIndex: 'phone',
+        key: 'phone'
+    },
+    {
+        title: 'Roles',
+        dataIndex: 'roles',
+        key: 'roles'
+    },
+]
+
 const UserTable = () => {
     return (
-        <div>
-            <Table />
+        <div className='table_block'>
+            <Table
+                columns={columns}
+            />
         </div>
     )
 }
