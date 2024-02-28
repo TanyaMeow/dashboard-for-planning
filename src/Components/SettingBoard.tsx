9cff82fc227e30fcf3350c628b713993e12e9293
thonyakina <thonyakina@zuzex.com>
2024-02-28 12:02:05 +0300

    Стилизация блока прогресса и блока пользователей входящий в отслеживаемую доску

--- src/Components/SettingBoard.tsx
+++ src/Components/SettingBoard.tsx
@@ -1,26 +1,36 @@
-import {Progress} from "antd";
+import {Progress, Avatar, Button} from "antd";
 import SelectionField from "./SelectionField";
 
 const SettingBoard = () => {
     return (
-        <div>
-            <div>
-                <h1>🏙️</h1>
-                <Progress type={"line"} percent={68}/>
-            </div>
-            <div>
-                <div>
-                    <img src="" alt=""/>
-                    <img src="" alt=""/>
-                    <img src="" alt=""/>
-                    <img src="" alt=""/>
-                    <div>
-                        +1
+        <div className='setting_block'>
+            <div className='boards_block'>
+                <div className='progress_bar'>
+                    <div className='board_icon'>
+                        <h1>🏙️</h1>
                     </div>
-                    <button>+ Add board</button>
+                    <Progress
+                        type={"line"}
+                        className={'indicator'}
+                        percent={68}
+                        strokeColor={'#6E6AF0'}
+                    />
+                    <p className='complete'>complete</p>
+                </div>
+                <div className='users_board'>
+                    <div className='users'>
+                        <Avatar.Group>
+                            <Avatar className='user_icon'>A</Avatar>
+                            <Avatar className='user_icon'>Б</Avatar>
+                            <Avatar className='user_icon'>В</Avatar>
+                            <Avatar className='user_icon'>Г</Avatar>
+                            <Avatar className='user_icon'>+1</Avatar>
+                        </Avatar.Group>
+                        <Button className='board_button'>+ Add board</Button>
+                    </div>
                 </div>
             </div>
-            <SelectionField />
+            <SelectionField/>
         </div>
     )
 }
