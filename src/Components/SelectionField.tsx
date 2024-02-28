eeab66232d750299516a97e3c1ed881b30ae3d45
thonyakina <thonyakina@zuzex.com>
2024-02-28 13:13:39 +0300

    Исправила стили, стилизовала блок с навигацией по доскам

--- src/Components/SelectionField.tsx
+++ src/Components/SelectionField.tsx
@@ -11,37 +11,41 @@ import setting from '../images/setting.svg';
 
 const SelectionField = () => {
     return (
-        <div>
-            <nav>
-                <NavLink to='/'>Description</NavLink>
-                <NavLink to='/board'>Board</NavLink>
-                <NavLink to='/notes'>Notes</NavLink>
-                <NavLink to='/table'>Table</NavLink>
-            </nav>
-            <div className='settings'>
-                <img src={reboot} alt=""/>
-                <img src={increase} alt=""/>
-                <img src={setting} alt=""/>
+        <div className='selection_block'>
+            <div className='selection_board-block'>
+                <nav className='selection_navigation'>
+                    <NavLink to='/' className='select_nav'>Description</NavLink>
+                    <NavLink to='/board' className='select_nav'>Board</NavLink>
+                    <NavLink to='/notes' className='select_nav'>Notes</NavLink>
+                    <NavLink to='/table' className='select_nav'>Table</NavLink>
+                </nav>
+                <div className='settings'>
+                    <img src={reboot} alt=""/>
+                    <img src={increase} alt=""/>
+                    <img src={setting} alt=""/>
+                </div>
             </div>
 
-            <Routes>
-                <Route
-                    path='/'
-                    element={<Description/>}
-                />
-                <Route
-                    path='/board'
-                    element={<Board/>}
-                />
-                <Route
-                    path='/notes'
-                    element={<Notes/>}
-                />
-                <Route
-                    path='/table'
-                    element={<UserTable/>}
-                />
-            </Routes>
+            <div className='board_content'>
+                <Routes>
+                    <Route
+                        path='/'
+                        element={<Description/>}
+                    />
+                    <Route
+                        path='/board'
+                        element={<Board/>}
+                    />
+                    <Route
+                        path='/notes'
+                        element={<Notes/>}
+                    />
+                    <Route
+                        path='/table'
+                        element={<UserTable/>}
+                    />
+                </Routes>
+            </div>
         </div>
     )
 }
