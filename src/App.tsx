be51abee5f900b65f3bbafd39eeb2cdaff35b007
thonyakina <thonyakina@zuzex.com>
2024-03-01 12:21:12 +0300

    Удален импорт модального окна

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,6 @@ import './styles/Common.scss';
 import Header from "./Components/Header";
 import Navigation from "./Components/Navigation";
 import SettingBoard from "./Components/SettingBoard";
-import UserTable from "./Components/UserTable";
 
 function App() {
   return (
